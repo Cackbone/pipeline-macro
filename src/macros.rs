1aa3bdefe65d3caf05a60d07594bb961ef0a84ab
Cedric Legendre <cedric.legendre@epitech.eu>
2019-08-22 19:44:57 +0200

    (feat) [pipeline/macros]: Add basic pipeline macro implementation

---
+++ src/macros.rs
@@ -0,0 +1,42 @@
+/// Helper to pass return of functions as parameter of other functions
+#[doc(hidden)]
+#[macro_export(local_inner_macros)]
+macro_rules! __pipeline_fn {
+    (($fn:ident($($args:expr),*)), $ret:expr) => {
+        $fn($ret $(,$arg)*);
+    };
+    ($fn:ident, $ret:expr) => {
+        $fn($ret)
+    };
+}
+
+/// Return new instance of pipeline struct
+///
+/// # Syntax:
+/// ```
+/// # use pipeline_macro::*;
+/// # struct InputType {}
+/// # struct OutputType {}
+/// # fn function1(i: InputType) -> InputType { i }
+/// # fn function2(i: InputType) -> OutputType { OutputType {} }
+/// pipeline! {
+///      InputType => function1 -> function2 => OutputType
+/// };
+/// ```
+#[macro_export]
+macro_rules! pipeline {
+    ($in:ty => $($fns:tt) -> + => $out:ty) => {
+        {{
+            Pipeline { fun : (| input: $in | {
+                let result = input;
+
+                $(
+                    let result = $crate::__pipeline_fn!($fns, result);
+                )*
+                return result;
+            })}
+        }}
+    }
+}
+
+
