137c7b30fbce6544b94f340d971423452c68abfd
Cedric Legendre <cedric.legendre@epitech.eu>
2019-08-22 19:05:28 +0200

    (feat) [pipeline struct]: Add pipeline struct with run method

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,25 @@
+
+/// A pipeline
+///
+/// # Generic parameters
+/// * `I` - Input type
+/// * `O` - Ouput type
+pub struct Pipeline<I, O> {
+    fun: fn(I) -> O
+}
+
+impl<I, O> Pipeline<I, O> {
+    /// Run this pipeline
+    ///
+    /// # Arguments
+    ///
+    /// * `input` - Input for this pipeline
+    pub fn run(&self, input: I) -> O {
+        (self.fun)(input)
+    }
+}
+
+
 #[cfg(test)]
 mod tests {
     #[test]
